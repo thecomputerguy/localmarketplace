8b662ea81c089ed2a9bceb78c33f128b9e52b116
Varun Sharma <varunsharma12@outlook.com>
2018-01-30 01:15:49 +0530

    localmarketplace: Added Changes for initializing database with the initial data.

--- src/main/java/com/localmarketplace/DatabaseInitializer.java
+++ src/main/java/com/localmarketplace/DatabaseInitializer.java
@@ -1,46 +1,49 @@
 package com.localmarketplace;
 
+import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.joda.time.Instant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.actuate.audit.AuditEventRepository;
+import org.springframework.stereotype.Component;
 
 import com.localmarketplace.domain.Customer;
 import com.localmarketplace.domain.Provider;
 import com.localmarketplace.domain.ProviderRating;
-import com.localmarketplace.service.AddressService;
-import com.localmarketplace.service.AuditEventService;
-import com.localmarketplace.service.CustomerService;
+import com.localmarketplace.domain.ServiceRequest;
+import com.localmarketplace.repository.AddressRepository;
+import com.localmarketplace.repository.CustomerRepository;
+import com.localmarketplace.repository.ProviderRatingRepository;
+import com.localmarketplace.repository.ProviderRepository;
+import com.localmarketplace.repository.ProviderReviewLogRepository;
+import com.localmarketplace.repository.ServiceAppointmentRepository;
+import com.localmarketplace.repository.ServiceCategoryRepository;
+import com.localmarketplace.repository.ServiceDeliveryOfferRepository;
+import com.localmarketplace.repository.ServiceProviderMapRepository;
+import com.localmarketplace.repository.ServiceRequestRepository;
+import com.localmarketplace.repository.UserRepository;
 import com.localmarketplace.service.MailService;
-import com.localmarketplace.service.ProviderRatingService;
-import com.localmarketplace.service.ProviderReviewLogService;
-import com.localmarketplace.service.ProviderService;
-import com.localmarketplace.service.ServiceAppointmentService;
-import com.localmarketplace.service.ServiceCategoryService;
-import com.localmarketplace.service.ServiceDeliveryOfferService;
-import com.localmarketplace.service.ServiceProviderMapService;
-import com.localmarketplace.service.ServiceRequestService;
 import com.localmarketplace.service.SocialService;
-import com.localmarketplace.service.UserService;
 
+@Component
 public class DatabaseInitializer implements CommandLineRunner {
 
-	@Autowired AddressService addressService;
-	@Autowired AuditEventService auditEventService;
-	@Autowired CustomerService cusotmerService;
+	@Autowired AddressRepository addressRepository;
+	@Autowired AuditEventRepository auditEventRepository;
+	@Autowired CustomerRepository customerRepository;
 	@Autowired MailService mailService;
-	@Autowired ProviderRatingService providerRatingService;
-	@Autowired ProviderReviewLogService providerReviewLogService;
-	@Autowired ProviderService providerService;
-	@Autowired ServiceAppointmentService serviceAppointmentService;
-	@Autowired ServiceCategoryService serviceCategoryService;
-	@Autowired ServiceDeliveryOfferService serviceDeliveryOfferService;
-	@Autowired ServiceProviderMapService serviceProviderMapService;
-	@Autowired ServiceRequestService serviceRequestService;
+	@Autowired ProviderRatingRepository providerRatingRepository;
+	@Autowired ProviderReviewLogRepository providerReviewLogRepository;
+	@Autowired ProviderRepository providerRepository;
+	@Autowired ServiceAppointmentRepository serviceAppointmentRepository;
+	@Autowired ServiceCategoryRepository serviceCategoryRepository;
+	@Autowired ServiceDeliveryOfferRepository serviceDeliveryOfferRepository;
+	@Autowired ServiceProviderMapRepository serviceProviderMapRepository;
+	@Autowired ServiceRequestRepository serviceRequestRepository;
 	@Autowired SocialService socialService;
-	@Autowired UserService userService;
+	@Autowired UserRepository userRepository;
 	
 	@Override
 	public void run(String... arg0) throws Exception {
@@ -59,14 +62,23 @@ public class DatabaseInitializer implements CommandLineRunner {
 		Customer customer6 = new Customer();
 		//Provider rating for rating the provider....
 		Set<ProviderRating> providerRatings = new HashSet<>();
-		ProviderRating providerRating = new ProviderRating();
-		providerRating.setAvgCommRating(5L);
-		providerRating.setAvgEtiRating(5L);
-		providerRating.setAvgOverallRating(5L);
-		providerRating.setAvgPriceRating(5L);
-		providerRating.setAvgProfRating(5L);
-		providerRating.setAvgPunctualityRating(5L);
-		providerRating.setLastUpdatedOn(Instant.now().getMillis());
+		ProviderRating providerRating1 = new ProviderRating();
+		providerRating1.setAvgCommRating(5L);
+		providerRating1.setAvgEtiRating(5L);
+		providerRating1.setAvgOverallRating(5L);
+		providerRating1.setAvgPriceRating(5L);
+		providerRating1.setAvgProfRating(5L);
+		providerRating1.setAvgPunctualityRating(5L);
+		providerRating1.setLastUpdatedOn(Instant.now().toEpochMilli());
+		
+		ProviderRating providerRating2 = new ProviderRating();
+		providerRating2.setAvgCommRating(5L);
+		providerRating2.setAvgEtiRating(5L);
+		providerRating2.setAvgOverallRating(4L);
+		providerRating2.setAvgPriceRating(4L);
+		providerRating2.setAvgProfRating(4L);
+		providerRating2.setAvgPunctualityRating(4L);
+		providerRating2.setLastUpdatedOn(Instant.now().toEpochMilli());
 		
 		provider1.setDescription("Plumbing services at your desk.");
 		provider1.setEmail("varunsharma12@outlook.com");
@@ -75,11 +87,53 @@ public class DatabaseInitializer implements CommandLineRunner {
 		provider1.setIsRegisteredOffice(false);
 		provider1.setLastName("sharma");
 		provider1.setMobileNumber(8446397639L);
+		provider1.setZip(411057L);
 		provider1.setOfficeAddress("hinjewadi");
 		
-		providerRating.setProvider(provider1);
-		providerRatings.add(providerRating);
+		providerRating1.setProvider(provider1);
+		providerRatings.add(providerRating1);
 		provider1.setRatings(providerRatings);
+		
+		provider2.setDescription("Plumbing services at your desk.");
+		provider2.setEmail("varunsharma12@outlook.com");
+		provider2.setFirstName("varun");
+		provider2.setIsIndividual(true);
+		provider2.setIsRegisteredOffice(false);
+		provider2.setLastName("sharma");
+		provider2.setMobileNumber(8446397639L);
+		provider2.setZip(441057L);
+		provider2.setOfficeAddress("hinjewadi");
+		
+		providerRating2.setProvider(provider1);
+		providerRatings.add(providerRating2);
+		provider2.setRatings(providerRatings);
+		
+		providerRepository.save(provider1);
+		providerRepository.save(provider2);
+		//Create customer objects and store them in database
+		Set<ServiceRequest> serviceRequests = new HashSet<>();
+		
+		customer1.setFirstName("Rahul");
+		customer1.setEmail("varunronaldo6976@gmail.com");
+		customer1.setLastName("Sharma");
+		customer1.setMobileNumber(8446397639L);
+		customer1.setServiceRequests(serviceRequests);
+		
+		ServiceRequest serviceRequest1 = new ServiceRequest();
+		serviceRequest1.setCustomer(customer1);
+		serviceRequest1.setExpectedStartTime(Instant.now());
+		serviceRequest1.setRequirementDescription("Need kitchen sink to be fixed.");
+		serviceRequest1.setServiceDeliveryOffers(null);
+		serviceRequest1.setServiceRequiredOn(Instant.now());
+		serviceRequest1.setTentativeEffortsRequiredInHours(5L);
+		serviceRequests.add(serviceRequest1);
+		customer1.setServiceRequests(serviceRequests);
+		
+		customerRepository.save(customer1);
+		
+		
+		
+		
 		
 	}
 
